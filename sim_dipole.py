f7e10994b5cd61a0c0de733b6ea119e5d7c18935
paolo-magnani <paolo.magnani@studenti.unimi.it>
2025-03-18 08:28:47 +0100

    added plots for presentation

--- sim_dipole.py
+++ sim_dipole.py
@@ -1,2 +1,12 @@
 # This library contains all the necessary functions to create a dipolar modulation in the simulated data
 import numpy as np
+
+class dipole:
+
+    d10 = 0.049
+    beta = 0.97
+
+    def dipole_dependence(self, E):
+        # energy in EeV
+        return self.d10*np.power(E/10, self.beta)
+    
