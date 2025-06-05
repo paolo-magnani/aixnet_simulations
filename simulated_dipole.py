228de179e73154308a2420c5bdd4d94b89a1f7ef
paolo-magnani <paolo.magnani@studenti.unimi.it>
2025-06-05 12:17:23 +0200

    final values for EPOS added

--- simulated_dipole.py
+++ simulated_dipole.py
@@ -34,12 +34,30 @@ def file_loader(estimator): # load files choosing the right estimator; further c
         print(len(data['dnn_xmax']))
         dd = {key: data[key] for key in data.files}
 
-    if estimator=='AixNet_EPOS_test':
+    elif estimator=='AixNet_EPOS_test':
         input_path = '/mnt/c/Users/paolo/Desktop/LAVORO/data_files/aixnet_epos_lhc_test_data.npz'
         data = np.load(input_path, allow_pickle=True)
         print(data.files)
         print(len(data['dnn_xmax']))
         dd = {key: data[key] for key in data.files}
+
+    elif estimator=='AixNet_EPOS_old':
+        input_path = '/mnt/c/Users/paolo/Desktop/LAVORO/Berenika/AixNet_EPOSold_refStSl_sims.csv'
+        kf = pd.read_csv(input_path)
+        dd = {}
+        dd['dnn_xmax'] = np.array(kf['Xmax'])
+        dd['mc_xmax'] = np.array(kf['Xmax_MC_int'])
+        dd['zenith'] = np.array(np.pi/2 - kf['Zenith']) # I use the same angle definition 
+        dd['energy'] = np.array(kf['Energy_MC']/1e18)
+        dd['primary'] = np.array(kf['Primary'])
+        dd['mass'] = np.empty_like(dd['energy'])
+        dd['mass'][np.where(dd['primary']==0)] = 1
+        dd['mass'][np.where(dd['primary']==1)] = 4
+        dd['mass'][np.where(dd['primary']==2)] = 16
+        dd['mass'][np.where(dd['primary']==3)] = 56
+
+        mask = (dd['mass']!=16)|((dd['dnn_xmax']<1000)&(dd['mass']==16))
+        dd = dict_cutter(dd, mask)
 
     elif estimator=='KAne':
         input_kane = '../berenika_dipole/dataset/KAne_EPOS_sims.csv'
@@ -420,7 +438,7 @@ class dipole_parameters:
     es = energy_spectrum()
 
     vec_d_r = np.arange(0.0001, 0.0201, step=0.0001)
-    vec_b_r = np.arange(0.1, 3.1, step=0.1)
+    vec_b_r = np.arange(0.1, 3.1, step=0.01)
 
     def dipole_func(self, name, E, d_r, beta_r, d_max):
         # energy in EeV
@@ -457,6 +475,95 @@ class dipole_parameters:
                     
                     dip = np.sum(Z_sum*self.es.spectrum_func_simpl(E))/np.sum(self.es.spectrum_func_simpl(E))
 
+                    val = (dip - dipole_values[i])
+                    if i>0: # Do not account the first bin!
+                        if val>0:
+                            chisq += (val/dipole_errors[1][i])**2
+                        else:
+                            chisq += (val/dipole_errors[0][i])**2
+
+                    results[f'{enebins[i]:.0f}'].append(dip)
+
+                
+                
+                results['chisq'].append(chisq)
+
+        for key, value in results.items():
+            results[key] = np.array(value)
+
+        best_d_r = results['d_r'][results['chisq']==np.min(results['chisq'])]
+        best_beta_r = results['beta_r'][results['chisq']==np.min(results['chisq'])]
+        print('\n Best d_r:', best_d_r)
+        print('Best beta_r:', best_beta_r)
+
+        return results, best_d_r, best_beta_r
+
+
+class dipole_parameters_emily:
+    # this class is given by f(Z,E)*J(E)*d(Z,E), and together they should minimize distance with the dipole values
+    # using Emily's recipe
+
+    def __init__(self, model='EPOS', dmax=3, e_steps=1000):
+        # this is used to evaluate the fraction f(Z,E)
+        self.mf = mass_fractions(model)
+        self.dmax = dmax
+        self.steps = e_steps
+        print('Model chosen:', model)
+        print('dipole cutoff:', self.dmax)
+        print('Number of energy steps in integration:', self.steps)
+
+    # this is used to evaluate spectrum
+    es = energy_spectrum()
+
+    vec_d_r = np.arange(0.0001, 0.0201, step=0.0001)
+    vec_b_r = np.arange(0.1, 3.1, step=0.1)
+
+    def dipole_func(self, name, E, d_r, beta_r, d_max):
+        # energy in EeV
+        dip = d_r*np.power((E/(charges[name])), beta_r)
+        cutoff = np.zeros_like(E) + d_max
+
+        return np.min([dip, cutoff], axis=0)
+
+    def evaluate(self):
+        print('Best parameter evaluation ...')
+        enebins = energies/1e18
+        results = {'d_r': [], 'beta_r': [], 'chisq':[], '4':[], '8': [], '16': [], '32': []}
+        counter = 0
+
+        # maybe this cycle can be optimized, Idk
+        for d_r in self.vec_d_r:
+            counter += 1
+            print('\r progress:', counter/2, '%', end='')
+
+            for beta_r in self.vec_b_r:
+                results['d_r'].append(d_r)
+                results['beta_r'].append(beta_r)
+
+                chisq = 0
+                
+                for i in range(len(enebins[:-1])):
+                    E = np.linspace(enebins[i], enebins[i+1], self.steps)
+                    Z_sum = np.zeros_like(E)
+                    mean_frac = 0
+                    dip = 0
+                    test = 0                  
+
+                    for name in charges:
+
+                        if name == 'Fe':
+                            Z_sum = self.dipole_func(name, E, d_r, beta_r, self.dmax)
+                            mean_frac=np.mean(1 - (self.mf.fraction_func('p', np.log10(E)+18) + self.mf.fraction_func('He', np.log10(E)+18) + self.mf.fraction_func('CNO', np.log10(E)+18)))
+                            dip += mean_frac*np.sum(Z_sum*self.es.spectrum_func_simpl(E))/np.sum(self.es.spectrum_func_simpl(E))
+                            test += mean_frac
+                        else:
+                            Z_sum = self.dipole_func(name, E, d_r, beta_r, self.dmax)
+                            mean_frac= np.mean(self.mf.fraction_func(name, np.log10(E)+18))
+                            dip += mean_frac*np.sum(Z_sum*self.es.spectrum_func_simpl(E))/np.sum(self.es.spectrum_func_simpl(E))
+                            test += mean_frac
+
+                                            
+                    print(test)
                     val = (dip - dipole_values[i])
                     if i>0: # Do not account the first bin!
                         if val>0:
